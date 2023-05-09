d726359d05c1a5eb9ea1f07f399c9589d20b5e5d
Bindholt <Cbindholt@gmail.com>
2023-05-09 11:24:00 +0200

    Refactored eventlisteners to function in module scope

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ import {showNavDropdown, showFullNav} from "./modules/nav/dropdown.js";
 window.addEventListener("load", main);
 
 function main(){
+	setEventListeners();
+}
+
+function setEventListeners() {
 	document.querySelector("#navExpandButton").addEventListener("click", showNavDropdown);
-	window.addEventListener("resize", showFullNav)
+	window.addEventListener("resize", showFullNav);
 }
