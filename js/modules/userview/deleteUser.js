42e1627adbc4f05174e28966ca39978b1ea61941
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2023-05-09 14:10:19 +0200

    fecthToDB

--- js/modules/userview/deleteUser.js
+++ js/modules/userview/deleteUser.js
@@ -1,25 +1,16 @@
 "use strict";
+import { sendFetchToDB } from "../../rest/fetch.js";
 import { updateUserGrid } from "./display.js";
 
-// window.addEventListener("load", main)
 
-const endpoint = "https://delfinen-4e935-default-rtdb.europe-west1.firebasedatabase.app/";
-
-function main() {
-  console.log("Hello");
-}
-
-async function deletePost(id) {
-  const response = await fetch(`${endpoint}users/${id}.json`, {
-    method: "DELETE",
-  });
+async function deleteUser(id) {
+  const response = await sendFetchToDB(`users/${id}.json`, "DELETE")
   if (response.ok) {
     console.log("Det er slettet");
     updateUserGrid();
   } else {
     console.error(`bad response : ${response.status} ${response.statusText}`);
   }
-  // update the post grid to display posts
 }
 
 export function showDeleteDialog(event) {
@@ -27,23 +18,20 @@ export function showDeleteDialog(event) {
 
   const userId = deleteBtn.dataset.id;
 
-  console.log(userId);
-
   document.querySelector("#dialog_delete_user").showModal();
 
   document.querySelector("#form_delete_post").addEventListener("submit", submitDelete);
   document.querySelector("#deleteCanceled").addEventListener("click", closeDelete);
 
   function submitDelete(event) {
-    console.log("Hej");
+    
     event.preventDefault();
     document.querySelector("#form_delete_post").removeEventListener("submit", submitDelete);
 
-    deletePost(userId);
+    deleteUser(userId);
 
     document.querySelector("#dialog_delete_user").close();
 
-    console.log("Det virker her til");
   }
 
   function closeDelete() {
