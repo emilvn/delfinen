6fbb20d79207e876ba266f6f0d56b12d99218669
emilvn <emilvnielsen@hotmail.com>
2023-05-10 11:04:09 +0200

    added seteventlisteners to main

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 import {showNavDropdown, showFullNav} from "./modules/nav/dropdown.js";
+import { generateNavHTML } from "./modules/nav/nav.js";
 
 window.addEventListener("load", main);
 
 function main(){
+	generateNavHTML();
+	setEventListeners();
+}
+
+function setEventListeners() {
 	document.querySelector("#navExpandButton").addEventListener("click", showNavDropdown);
 	window.addEventListener("resize", showFullNav);
 }
