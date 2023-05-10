7dd8cc8d776858addc31293065cebe62cad4d8fa
Bindholt <Cbindholt@gmail.com>
2023-05-10 10:26:49 +0200

    Revert "Merge branch 'main' of https://github.com/emilvn/delfinen"
    This reverts commit 0774c576b206f5b22d5a4fb5c30db87cb6dc5127, reversing
    changes made to 42e1627adbc4f05174e28966ca39978b1ea61941.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
 import {showNavDropdown, showFullNav} from "./modules/nav/dropdown.js";
-import { generateNavHTML } from "./modules/nav/nav.js";
 
 window.addEventListener("load", main);
 
 function main(){
-	generateNavHTML();
-	setEventListeners();
-}
-
-function setEventListeners() {
 	document.querySelector("#navExpandButton").addEventListener("click", showNavDropdown);
 	window.addEventListener("resize", showFullNav);
 }
