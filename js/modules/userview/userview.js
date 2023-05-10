2128defe24dee5cf08db013b946f2b5b856dfabb
VARattleff <viktoraugustrattleff@gmail.com>
2023-05-10 11:41:44 +0200

    opdateret med js validet

--- js/modules/userview/userview.js
+++ js/modules/userview/userview.js
@@ -20,16 +20,41 @@ function setEventListeners() {
     });
 }
 
-async function submitNewTrainingTime(event) {
-    event.preventDefault();
-    const disciplin = document.getElementById("disciplin").value;
-    const datetime = document.getElementById("date-time").value;
-    const seconds = document.getElementById("seconds").value;
-    const newTrainingTime = {disciplin, datetime, seconds};
-    const url = `trainingtimes/${disciplin}.json`;
-    const res = await sendFetchToDB(url, 'POST', newTrainingTime);
-    if(res.ok) {
-        const data = await res.json();
-        return data;
+async function checkTrainingTimeFields(disciplin, datetime, seconds) {
+  if (!disciplin || !datetime || !seconds) {
+    const completedMessage = document.querySelector("#add_training_time_form");
+    completedMessage.classList.remove('valid');
+    completedMessage.classList.add('invalid');
+    return false;
+  } else {
+    const completedMessage = document.querySelector("#add_training_time_form");
+    completedMessage.classList.remove('invalid');
+    completedMessage.classList.add('valid');
+    return true;
   }
-} 
+}
+
+async function submitNewTrainingTime(event) {
+  event.preventDefault();
+  const disciplin = document.getElementById("disciplin").value;
+  const datetime = document.getElementById("date-time").value;
+  const seconds = document.getElementById("seconds").value;
+
+  const fieldsValid = await checkTrainingTimeFields(disciplin, datetime, seconds);
+
+  if (!fieldsValid) {
+    return;
+  }
+
+  const newTrainingTime = {disciplin, datetime, seconds};
+  const url = `trainingtimes/${disciplin}.json`;
+  const res = await sendFetchToDB(url, 'POST', newTrainingTime);
+
+  if (res.ok) {
+    console.info('sendt');
+    const data = await res.json();
+    return data;
+  }
+}
+
+
