9c703f5497eab0c39d9122972cf0c7fd102ee18c
VARattleff <viktoraugustrattleff@gmail.com>
2023-05-10 18:54:53 +0200

    opdateret js med mere læselig kode og function til opendialog

--- js/modules/userview/userview.js
+++ js/modules/userview/userview.js
@@ -6,25 +6,32 @@ function main(){
     setEventListeners();
 }
 
+function showTrainingTimeDialog(event) {
+  const form = document.querySelector("#add_training_time_form");
+  form.dataset.id = event.target.dataset.id;
+  const dialog = document.querySelector("#add_training_time");
+  dialog.showModal();
+}
+
+
 function setEventListeners() {
+  
   document
     .querySelector("#add_training_time_form")
-    .addEventListener("submit", function (event) {submitNewTrainingTime(event)});
+    .addEventListener("submit", submitNewTrainingTime);
 
   document
-    .querySelector("#add_training_time_form")
-    .addEventListener("keydown",  function (event) {
-      if (event.keyCode === 13) {
-         submitNewTrainingTime(event);
-      }
+    .querySelector("#close_button")
+    .addEventListener("click", function () {
+      const dialog = document.querySelector("#add_training_time");
+      dialog.close();
     });
 
-     document
-      .querySelector("#close_button")
-      .addEventListener("click", function () {
-        const dialog = document.getElementById("add_training_time");
-        dialog.close();
-      });
+  document
+  .querySelector("#open_training_time_dialog_button")
+  .addEventListener("click", showTrainingTimeDialog);
+
+
 }
 
  function checkTrainingTimeFields(disciplin, datetime, seconds) {
@@ -41,26 +48,34 @@ function setEventListeners() {
   }
 }
 
-async function submitNewTrainingTime(event) {
+function submitNewTrainingTime(event) {
   event.preventDefault();
-  const disciplin = document.getElementById("disciplin").value;
-  const datetime = document.getElementById("date-time").value;
-  const seconds = document.getElementById("seconds").value;
+  const form = event.target;
+  const uid = form.dataset.id;
+  
+  const category = form['category'].value; 
+  const datetime = form['date-time'].value; 
+  const seconds = form['seconds'].value;
+  const fieldsValid = checkTrainingTimeFields(category, datetime, seconds);
 
-  const fieldsValid = await checkTrainingTimeFields(disciplin, datetime, seconds);
-
-  if (!fieldsValid) {
-    return;
-  }
-
-  const newTrainingTime = {disciplin, datetime, seconds};
-  const url = `trainingtimes/${disciplin}.json`;
-  const res = await sendFetchToDB(url, 'POST', newTrainingTime);
-
-  if (res.ok) {
-    const data = await res.json();
-    return data;
+  if (fieldsValid) {
+    const newTrainingTime = {
+      uid: uid,
+      date: datetime,
+      time: seconds
+    };
+    addTrainingtime(category, newTrainingTime);
   }
 }
 
 
+async function addTrainingtime (category, trainingtimeData){
+  const uri = `trainingstimes/${category}.json`;
+  const res = await sendFetchToDB(uri, 'POST', trainingtimeData);
+  if (res.ok){
+    console.log('Training time added');
+  }
+  else {
+    console.error(`Bad response at addTrainingtime: ${res.status} ${res.statusText}`)
+  }
+}
