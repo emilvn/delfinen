7e54bc22f86d8fc3d709747548e0b95a18ed5669
VARattleff <viktoraugustrattleff@gmail.com>
2023-05-11 14:42:47 +0200

    opdateret js til at vise video

--- js/main.js
+++ js/main.js
@@ -6,9 +6,20 @@ window.addEventListener("load", main);
 function main(){
 	generateNavHTML();
 	setEventListeners();
+	displayVideo();
 }
 
 function setEventListeners() {
 	document.querySelector("#navExpandButton").addEventListener("click", showNavDropdown);
 	window.addEventListener("resize", showFullNav);
+}
+
+function displayVideo(){
+	const videoBg = document.createElement('video');
+	videoBg.src = 'data/videos/water.mp4';
+	videoBg.muted = true;
+	videoBg.loop = true;
+	videoBg.autoplay = true;
+	videoBg.setAttribute('id', 'videoBackground');
+	document.querySelector('#homeMainScreen').appendChild(videoBg);
 }
