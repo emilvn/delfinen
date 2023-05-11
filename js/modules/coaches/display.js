6c1c04a274808447256cec4de0e9b2823e01fad3
emilvn <emilvnielsen@hotmail.com>
2023-05-11 11:23:20 +0200

    coach display functions added

--- js/modules/coaches/display.js
+++ js/modules/coaches/display.js
@@ -1,7 +1,41 @@
 import {getCoaches} from "../../rest/fetch.js";
+import {capitalize} from "../preparation/formatting.js";
 
 let coachArr;
 
-export function updateCoachesGrid(){
+export async function updateCoachesGrid(){
+    coachArr = await getCoaches();
+    showCoaches(coachArr);
+}
 
+function showCoaches(coaches){
+    document.querySelector("#coach-grid").innerHTML = "";
+    for(const coach of coaches){
+        showCoach(coach);
+    }
+}
+
+function showCoach(coach){
+    const coachContainer = document.querySelector("#coach-grid");
+    const myHTML = /*html*/`
+    <article>
+        <div class="coach-image">
+            <img src="${coach["image"]}" alt="">    
+        </div>
+        <div class="coach-general-info">
+            <h2>${coach["name"]} - ${capitalize(coach["team"])} træner</h2>
+            <div>
+                <h3>Kontaktinformation:</h3>
+                <ul>
+                    <li>Tlf: ${coach["phone"]}</li>
+                    <li>Email: ${coach["email"]}</li>
+                </ul>
+            </div>
+        </div>
+        <div class="coach-about">
+            <p>${coach["about"]}</p>
+        </div>
+    </article>
+    `;
+    coachContainer.insertAdjacentHTML("beforeend", myHTML);
 }
--- js/modules/coaches/main.js
+++ js/modules/coaches/main.js
@@ -1,5 +1,7 @@
+import {updateCoachesGrid} from "./display.js";
+
 window.addEventListener("load", main);
 
 function main(){
-
+    updateCoachesGrid();
 }
