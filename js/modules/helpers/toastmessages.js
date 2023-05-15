6c4ef41dc93a514313e1af93a1c5dfa25cc70b69
emilvn <emilvnielsen@hotmail.com>
2023-05-15 13:12:14 +0200

    added toast container to all pages

---
+++ js/modules/helpers/toastmessages.js
@@ -0,0 +1,13 @@
+/* ========== SUCCESS/ERROR TOAST MESSAGE ========== */
+//type is success or error
+export function showToastMessage(message, type) {
+	const toastContainer = document.querySelector("#toast-container");
+	const toast = document.createElement("div");
+	toast.textContent = message;
+	toast.classList.add("toast", type);
+	toastContainer.appendChild(toast);
+	toast.classList.add("fade-out");
+	setTimeout(() => {
+		toastContainer.removeChild(toast);
+	}, 3000);
+}
