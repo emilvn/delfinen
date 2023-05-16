e4a8a9bf0210ed4a529d4c76d058be63eae00f34
emilvn <emilvnielsen@hotmail.com>
2023-05-16 10:20:24 +0200

    added calculateAge

--- js/modules/helpers/helpers.js
+++ js/modules/helpers/helpers.js
@@ -5,3 +5,17 @@ export function resetLocalStorage(str) {
 export function reDirect(pageName) {
 	window.location = `${pageName}.html`;
 }
+
+export function calculateAge(birthdate){
+	const dateOfBirth = new Date(birthdate);
+	const today = new Date();
+
+	let age = today.getFullYear() - dateOfBirth.getFullYear();
+	const monthDifference = today.getMonth() - dateOfBirth.getMonth();
+	const dayDifference = today.getDate() - dateOfBirth.getDate();
+
+	if(monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)){
+		age--;
+	}
+	return age;
+}
