657eadc36e8562d751e650dad28a479fec13cc26
emilvn <emilvnielsen@hotmail.com>
2023-05-17 17:34:13 +0200

    now shows the top 5 competitors

--- js/modules/competitiontimes/display.js
+++ js/modules/competitiontimes/display.js
@@ -3,5 +3,15 @@ import {findTop5Competitors} from "./filter.js";
 
 export async function displayTop5Competitors(category, competition){
     const competitors = await findTop5Competitors(category, competition);
-    console.log(competitors);
+
+    for(let i = 0; i<competitors.length; i++){
+        document.querySelector(`#competitor-number-${i+1}__competitor`)
+            .innerHTML = /*html*/`
+            <h3>${competitors[i]["name"]}</h3>
+            <ul>
+                <li>Placering: ${competitors[i]["position"]}</li>
+                <li>Tid: ${competitors[i]["time"]}</li>
+            </ul>
+            `;
+    }
 }
