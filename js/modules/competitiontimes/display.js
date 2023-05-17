747f6a66cb61b388d4d56448a4c97c438d02b90d
emilvn <emilvnielsen@hotmail.com>
2023-05-17 17:06:07 +0200

    added some styling

--- js/modules/competitiontimes/display.js
+++ js/modules/competitiontimes/display.js
@@ -1,28 +1,7 @@
-import {getAllUsers} from "../../rest/fetch.js";
-import {competitionsObj} from "./options.js";
+import {findTop5Competitors} from "./filter.js";
 
 
 export async function displayTop5Competitors(category, competition){
-    const competitors = await getCompetitorsInCompetition(category, competition);
-    competitors.sort((a,b) => a.position - b.position);
-    competitors.splice(5);
+    const competitors = await findTop5Competitors(category, competition);
     console.log(competitors);
-}
-
-
-async function getCompetitorsInCompetition(category, competition){
-    const members = await getAllUsers();
-    const memberIdsInCompetition = [];
-    const competitionCategory = competitionsObj[competition][category];
-
-    for(const memberID in competitionCategory){
-        memberIdsInCompetition.push(memberID);
-    }
-    const competitors = members.filter(member => memberIdsInCompetition.includes(member.id));
-    for(const competitor of competitors){
-        const competitorCompetitionData = competitionCategory[competitor["id"]];
-        competitor["position"] = competitorCompetitionData["position"];
-        competitor["time"] = competitorCompetitionData["time"];
-    }
-    return competitors;
 }
---
+++ js/modules/competitiontimes/filter.js
@@ -0,0 +1,28 @@
+import {getAllUsers} from "../../rest/fetch.js";
+import {competitionsObj} from "./options.js";
+
+
+export async function findTop5Competitors(category, competition){
+    const competitors = await getCompetitorsInCompetition(category, competition);
+    competitors.sort((a,b) => a.position - b.position);
+    competitors.splice(5);
+    return competitors;
+}
+
+
+async function getCompetitorsInCompetition(category, competition){
+    const members = await getAllUsers();
+    const memberIdsInCompetition = [];
+    const competitionCategory = competitionsObj[competition][category];
+
+    for(const memberID in competitionCategory){
+        memberIdsInCompetition.push(memberID);
+    }
+    const competitors = members.filter(member => memberIdsInCompetition.includes(member.id));
+    for(const competitor of competitors){
+        const competitorCompetitionData = competitionCategory[competitor["id"]];
+        competitor["position"] = competitorCompetitionData["position"];
+        competitor["time"] = competitorCompetitionData["time"];
+    }
+    return competitors;
+}
