cb28455d3a7c7fa8497ca91a0305cc7088ee2b3d
Bindholt <Cbindholt@gmail.com>
2023-05-23 14:57:22 +0200

    q

--- js/modules/trainingtimes/main.js
+++ js/modules/trainingtimes/main.js
@@ -17,5 +17,7 @@ function setEventListeners() {
     document.querySelectorAll("input[name='teamselect']").forEach((input) => {
         input.addEventListener('change', displayTrainingTimes);
     });
-    document.querySelector("#training-time-search").addEventListener("change", (event) => searchForName(event, globalDisplayArray))
+    document.querySelector("#training-time-search").addEventListener("search", (event) => {
+        searchForName(event, globalDisplayArray);
+    });
 }
