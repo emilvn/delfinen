bdcfc32fb8c20ac9f3dfa0f67a8838e812f0462d
VARattleff <viktoraugustrattleff@gmail.com>
2023-05-24 00:12:24 +0200

    opdateret med anchor animation

--- js/modules/index/timeline.js
+++ js/modules/index/timeline.js
@@ -13,10 +13,11 @@ function addAnimation() {
   const styleElement = document.createElement('style');
   styleElement.innerHTML = `
     .timeline::before {
-      animation: timeline-animation 10s 1 linear;
+      animation: timeline-animation 8s 1 linear;
+    }
+    .anchor {
+      animation: anchor-animation 10s 1 linear;
     }
   `;
   document.head.appendChild(styleElement);
 }
-
-
